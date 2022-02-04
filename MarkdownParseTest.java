8f2daf349db23db35d78ba32795eeca17fc165be
Goldar Luu <gluu41@gmail.com>
2022-02-03 16:16:50 -0800

    Failing Format2.md
    Formating a link with a space in between the () and []

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -45,5 +45,11 @@ public class MarkdownParseTest {
         assertEquals("Testing for incorrectlink.md", List.of(), links);
     }
     
-
+    @Test
+    public void testFormat2() throws IOException {
+        Path fileName = Path.of("format2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertNotEquals(List.of(), links);
+    }
 }
