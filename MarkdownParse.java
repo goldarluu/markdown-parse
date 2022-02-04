38f67e0fd7110d97f0f5feef671ebf90117f2b82
Goldar Luu <gluu41@gmail.com>
2022-02-03 16:17:04 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -26,6 +26,12 @@ public class MarkdownParse {
                 currentIndex = closeParen + 1;
                 continue;
             }
+            if (openParen - nextCloseBracket > 1) 
+            {
+                currentIndex  = closeParen + 1;
+                continue;
+            }
+            
             
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
